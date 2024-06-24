f0bce8e5c40521a52cd39cd805b2f9f95380f4b0
Teeuzim <matheusvaz01@yahoo.com.br>
2024-06-24 17:31:01 -0300

    bubbleMenuOk

--- src/components/BubbleButton.tsx
+++ src/components/BubbleButton.tsx
@@ -1,13 +1,14 @@
-import { Children, ComponentProps, ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 
-export interface BubbleButtonProps extends ComponentProps<'button'> {
+export interface BubbleButtonProps extends ComponentProps<"button"> {
   children: ReactNode;
 }
 
 export function BubbleButton(props: BubbleButtonProps) {
   return (
-    <button className="p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 ">
-        {props.children}
-  </button>
-  )
+    <button
+      className="p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:text-violet-400"
+      {...props}
+    />
+  );
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -18,6 +18,8 @@ import {
   RxChevronDown,
   RxChatBubble,
 } from "react-icons/rx";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
+import * as Popover from "@radix-ui/react-popover";
 import { BubbleButton } from "./BubbleButton";
 
 const lowlight = createLowlight();
@@ -46,34 +48,101 @@ export function Editor() {
         className="max-w-[700px] mx-auto pt-16 prose prose-invert prose-violet"
         editor={editor}
       />
-      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      {editor && (
+        <FloatingMenu
+          editor={editor}
+          className="bg-zinc-700 py-2 px-1 shadow-xl border gap-1 border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex flex-col divide-x divide-zinc-600"
+          shouldShow={({ state }) => {
+            const { $from } = state.selection;
+
+            const currentLineText = $from.nodeBefore?.textContent;
+
+            return currentLineText === "/";
+          }}
+        >
+          <button className="flex items-center gap-2 p-1 rounded min-w-[200px] hover:bg-zinc-600">
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              alt="Text"
+              className="w-12 bnorder border-zinc-600 rounded "
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xm text-zinc-400">
+                Just start writing with plain text.
+              </span>
+            </div>
+          </button>
+
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[200px] hover:bg-zinc-600"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+          >
+            <img
+              src="http://www.notion.so/images/blocks/header.57a7576a.png"
+              alt="Heading"
+              className="w-12 bnorder border-zinc-600 rounded "
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Heading 1</span>
+              <span className="text-xm text-zinc-400">
+                Big section heading.
+              </span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
       {editor && (
         <BubbleMenu
           className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600"
           editor={editor}
         >
-          <BubbleButton>
-            Text
-            <RxChevronDown className="w-4 h-4" />
+           <BubbleButton>
+            <Popover.Root>
+              <Popover.Trigger className="flex items-center gap-1">
+                Text
+                <RxChevronDown className="w-4 h-4" />
+              </Popover.Trigger>
+              <Popover.Portal>
+                <Popover.Content className="bg-zinc-200 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600 p-2">
+                  Some more info…
+                  <Popover.Arrow className="fill-current text-zinc-200" />
+                </Popover.Content>
+              </Popover.Portal>
+            </Popover.Root>
           </BubbleButton>
           <BubbleButton>
-            Comment
             <RxChatBubble className="w-4 h-4" />
+            Comment
           </BubbleButton>
           <div className="flex items-center">
-            <BubbleButton>
-              <RxFontBold className="w-4 h-4" />{" "}
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              data-active={editor.isActive("bold")}
+            >
+              <RxFontBold className="w-4 h-4" />
             </BubbleButton>
 
-            <BubbleButton>
-              <RxFontItalic className="w-4 h-4" />{" "}
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              data-active={editor.isActive("italic")}
+            >
+              <RxFontItalic className="w-4 h-4" />
             </BubbleButton>
 
-            <BubbleButton>
-              <RxStrikethrough className="w-4 h-4" />{" "}
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              data-active={editor.isActive("strike")}
+            >
+              <RxStrikethrough className="w-4 h-4" />
             </BubbleButton>
 
-            <BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleCode().run()}
+              data-active={editor.isActive("code")}
+            >
               <RxCode className="w-4 h-4" />
             </BubbleButton>
           </div>
